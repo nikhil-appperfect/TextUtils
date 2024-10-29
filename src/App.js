9c80c820dd0488d979100c80bede24a3c2e10474
nikhil-appperfect <nikhil@appperfect.com>
2024-10-29 12:33:56 +0530

    removed routing

--- src/App.js
+++ src/App.js
@@ -2,54 +2,42 @@ import "./App.css";
 import Alert from "./components/Alert";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
-import About from './components/About'
-import React, { useState } from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom"
+// import About from "./components/About";
+import React, { useState } from "react";
 
 function App() {
-  const [mode, setMode] = useState('light');
+  const [mode, setMode] = useState("light");
   const [alert, setAlert] = useState(null);
 
-  const triggerAlert = (message, type) =>{
+  const triggerAlert = (message, type) => {
     setAlert({
       msg: message,
-      typ: type
-    })
-    setTimeout(()=>{setAlert(null);}, 1500);
-  }
+      typ: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
 
   const toggleMode = () => {
-    if(mode==='dark'){
-        setMode('light');
-        document.body.style.backgroundColor = 'white'
-        triggerAlert('Light mode has been enabled!', 'success');
-      }
-      else{
-        setMode('dark');
-        document.body.style.backgroundColor = '#042743'
-        triggerAlert('Dark mode has been enabled!', 'success');
+    if (mode === "dark") {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      triggerAlert("Light mode has been enabled!", "success");
+    } else {
+      setMode("dark");
+      document.body.style.backgroundColor = "#042743";
+      triggerAlert("Dark mode has been enabled!", "success");
     }
-}
+  };
   return (
     <>
-    <Router>
-      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
-      <Alert alert={alert}/>
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
       <div className="container my-3">
-        <Switch>
-          <Route exact path="/about">
-            <About />
-          </Route>
-          <Route exact path="/">
-            <TextForm triggerAlert={triggerAlert} heading="Enter text here to analyze below" mode={mode}/>
-          </Route>
-        </Switch>
+        {/* <About /> */}
+        <TextForm triggerAlert={triggerAlert} heading="Enter text here to analyze below" mode={mode} />
       </div>
-      </Router>
     </>
   );
 }
